98d1ca02410a1655e04913b4c3c3c0dbb741a044
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-04 11:50:01 +0530

    sum of A and B in java

---
+++ Java Programming/1. Variable & Data Types/E1SumofAnB.java
@@ -0,0 +1,12 @@
+public class E1SumofAnB {
+    
+    public static void main(String[] args) {
+        
+        int A = 10;
+        int B = 15;
+
+        int sum = A + B;
+
+        System.out.println("The sum of " + A + " and " + B + " is: " + sum);
+    }
+}
