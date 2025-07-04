338dfe5574d090a761e40f5da37088d2ef71f888
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-04 11:11:51 +0530

    printing the pattern

---
+++ Java Programming/1. Variable & Data Types/B1PrintPattern.java
@@ -0,0 +1,9 @@
+public class B1PrintPattern {
+    
+    public static void main(String[] args) {
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
