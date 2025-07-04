45f80b978090adf96e12193962788084d97f61bc
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-04 11:25:37 +0530

    variables in java

---
+++ Java Programming/1. Variable & Data Types/C1Variables.java
@@ -0,0 +1,16 @@
+public class C1Variables {
+    
+    public static void main(String[] args) {
+        
+        String name = "Aditya Kumar";
+        int age = 19;
+        double cgpa = 8.3;
+
+        System.out.println(name + " is "+age+" years old with "+cgpa+" cgpa");
+
+        age = 20;
+        cgpa = 8.5;
+
+        System.out.println(name + " is "+age+" years old with "+cgpa+" cgpa");
+    }
+}
