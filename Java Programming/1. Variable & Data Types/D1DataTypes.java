0456c4ee5347cfa7e750cd6b87c0ace7fd42ee65
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-04 11:43:05 +0530

    primitive data types in java

---
+++ Java Programming/1. Variable & Data Types/D1DataTypes.java
@@ -0,0 +1,28 @@
+public class D1DataTypes {
+    
+    public static void main(String[] args) {
+        byte b = 8;
+        System.out.println(b);
+
+        char ch = 'a';
+        System.out.println(ch);
+
+        boolean isOk = true;
+        System.out.println(isOk);
+
+        float price = (float)10.5;
+        System.out.println(price);
+
+        int number = 500;
+        System.out.println(number);
+
+        long students = 1700000;
+        System.out.println(students);
+
+        short marks = 700;
+        System.out.println(marks);
+
+        double pi = 3.141592653589793;
+        System.out.println(pi);
+    }
+}
