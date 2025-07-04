0ec0bdaf0a23846407a7fd6caa7060b5dbf4c2de
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-04 10:58:36 +0530

    print in java

--- Java Programming/1. Variable & Data Types/OnedotJavaBasics.java
+++ Java Programming/1. Variable & Data Types/OnedotJavaBasics.java
@@ -3,5 +3,7 @@ public class OnedotJavaBasics {
     public static void main(String[] args) {
         
         System.out.println("Hello Aditya, Welcome to Java");
+        System.out.print("Hellllo ");
+        System.out.print("G, How are you?");
     }
 }
