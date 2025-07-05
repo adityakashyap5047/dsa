123ed96dfd4470a6c28123896f1efa68ae5a9bd2
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-05 15:19:57 +0530

    find the value in java

---
+++ Java Programming/3. Conditional Statement/N3FindValue.java
@@ -0,0 +1,12 @@
+public class N3FindValue {
+   
+    public static void main(String args[]) {
+        int a = 63, b = 36;
+
+        boolean x = (a < b ) ? true : false; // false
+        int y = (a > b ) ? a : b; //63
+
+        System.out.println("X: " + x);
+        System.out.println("Y: " + y);
+    }
+}
