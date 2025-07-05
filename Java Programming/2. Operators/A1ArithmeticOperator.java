3c5d3c6a0c32cdf696b9b99ee129e542d3da0966
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-05 11:51:00 +0530

    binary arithmetic operator in java

---
+++ Java Programming/2. Operators/A1ArithmeticOperator.java
@@ -0,0 +1,13 @@
+public class A1ArithmeticOperator {
+    
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 5;
+
+        System.out.println("Addition: " + (A + B));
+        System.out.println("Subtraction: " + (A - B));
+        System.out.println("Multiplication: " + (A * B));
+        System.out.println("Division: " + (A / B));
+        System.out.println("Modulus: " + (A % B));
+    }
+}
