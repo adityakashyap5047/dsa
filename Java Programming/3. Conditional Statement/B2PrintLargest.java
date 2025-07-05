80ce4d679699ac8c027a8c12af7619391a953f63
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-05 13:52:55 +0530

    larger than two number

---
+++ Java Programming/3. Conditional Statement/B2PrintLargest.java
@@ -0,0 +1,14 @@
+public class B2PrintLargest {
+    
+    public static void main(String[] args) {
+        
+        int A = 10;
+        int B = 5;
+
+        if (A >= B){
+            System.out.println(A + " is larger than or equal to " + B);
+        } else {
+            System.out.println(A + " is smaller than " + B);
+        }
+    }
+}
