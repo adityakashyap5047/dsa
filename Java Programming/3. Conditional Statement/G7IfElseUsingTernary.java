3a69bd092fc6770a6fd13b25eab56c5e8d25ed98
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-05 14:44:58 +0530

    even and odd using ternary operator

---
+++ Java Programming/3. Conditional Statement/G7IfElseUsingTernary.java
@@ -0,0 +1,11 @@
+public class G7IfElseUsingTernary {
+    
+    public static void main(String[] args) {
+        
+        int num = 8;
+
+        String type = ((num % 2) == 0) ? "Even" : "Odd";
+
+        System.out.println(num + " is an " + type + " number.");
+    }
+}
