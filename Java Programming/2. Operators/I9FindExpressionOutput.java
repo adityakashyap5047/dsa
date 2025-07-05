45db327988666580f0641b55444fcf362188db0e
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-05 12:49:42 +0530

    finding the output  of the program

---
+++ Java Programming/2. Operators/I9FindExpressionOutput.java
@@ -0,0 +1,11 @@
+public class I9FindExpressionOutput {
+    
+    public static void main(String[] args){
+        int x = 10, y = 5;
+
+        int exp1 = (y * (x / y + x / y));   // (5 * (10/5 + 10/5)) = (5 * (2 + 2)) = (5 * 4) = 20
+        int exp2 = (y * x / y + y * x / y); // (5 * 10 / 5 + 5 * 10 / 5) = (50 / 5 + 50 / 5) = (10 + 10) = 20
+        System.out.println(exp1);
+        System.out.println(exp2);
+    }
+}
