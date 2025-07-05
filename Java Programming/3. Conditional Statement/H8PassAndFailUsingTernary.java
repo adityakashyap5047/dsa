57c4af6e8d524429a12751c2d8d0857a317900fd
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-05 14:47:08 +0530

    pass and fail using ternary operator

---
+++ Java Programming/3. Conditional Statement/H8PassAndFailUsingTernary.java
@@ -0,0 +1,10 @@
+public class H8PassAndFailUsingTernary {
+    
+    public static void main(String[] args) {
+        int marks = 50;
+
+        String status = (marks >= 33) ? "Pass" : "Fail";
+
+        System.out.println("The student got marks: " + marks + " Therefore, he is " + status);
+    }
+}
