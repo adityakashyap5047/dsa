f3a513ff426f1c6533ea1b3f39b3bb7b722027cd
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-05 22:36:38 +0530

    while loop in java

---
+++ Java Programming/4. Loops/A4WhileLoop.java
@@ -0,0 +1,12 @@
+public class A4WhileLoop {
+    
+    public static void main(String s[]){
+
+        int counter = 0;
+
+        while (counter < 100) {
+            System.out.println("Hello World: " + (counter + 1));
+            counter++;
+        }
+    }
+}
