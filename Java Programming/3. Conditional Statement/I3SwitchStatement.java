4bc25a57435784a2b61df8b7f6db13156d93310c
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-05 14:55:18 +0530

    switch case statement in java

---
+++ Java Programming/3. Conditional Statement/I3SwitchStatement.java
@@ -0,0 +1,21 @@
+public class I3SwitchStatement {
+    
+    public static void main(String[] args) {
+        int num = 9;
+
+        switch (num) {
+            case 1:
+                System.out.println("Samosa");
+                break;
+            case 2:
+                System.out.println("Burger");
+                break;
+            case 3:
+                System.out.println("Pizza");
+                break;
+            default:
+                System.out.println("You are dreaming");
+                break;
+        }
+    }
+}
