3b429ade893e22598b42877a9c6b11896dfc94e3
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-05 12:02:30 +0530

    relational operator in java

---
+++ Java Programming/2. Operators/B2RelationalOperator.java
@@ -0,0 +1,14 @@
+public class B2RelationalOperator {
+    
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 10;
+        
+        System.out.println("A is equal to B: " + (A == B));
+        System.out.println("A is not equal to B: " + (A != B));
+        System.out.println("A is greater than B: " + (A > B));
+        System.out.println("A is less than B: " + (A < B));
+        System.out.println("A is greater than or equal to B: " + (A >= B));
+        System.out.println("A is less than or equal to B: " + (A <= B));
+    }
+}
