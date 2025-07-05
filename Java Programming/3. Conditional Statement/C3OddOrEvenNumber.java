acc42cfa3a49e59d8aa35b5c6445cccbc5edf48f
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-05 13:55:39 +0530

    even or odd in java

---
+++ Java Programming/3. Conditional Statement/C3OddOrEvenNumber.java
@@ -0,0 +1,12 @@
+public class C3OddOrEvenNumber {
+    
+    public static void main(String[] args) {
+        int num = 103;
+
+        if(num % 2 == 0){
+            System.out.println(num + " is an Even number");
+        } else {
+            System.out.println(num + " is an Odd number");
+        }
+    }
+}
