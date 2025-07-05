e82293293183c9374a3b9c26628908645c433eff
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-05 12:41:47 +0530

    finding the output of an expression

---
+++ Java Programming/2. Operators/G7FindExpressionOutput.java
@@ -0,0 +1,12 @@
+public class G7FindExpressionOutput {
+    
+    public static void main(String[] args){
+        int x, y, z;
+
+        x = y = z = 2;
+        x += y;         // x = 2 + 2 = 4
+        y -= z;         // y = 2 - 2 = 0
+        z /= (x + y);   // z = 2 / (4 + 0) = 2 / 4 = 0
+        System.out.println(x + " " + y + " " + z);
+    }
+}
