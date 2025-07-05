1cdfd7f07d89c9f77da3a6097481dcff19434f1d
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-05 23:40:37 +0530

    finding no. of Iteration in java

---
+++ Java Programming/4. Loops/N4NoOfLoopRun.java
@@ -0,0 +1,9 @@
+public class N4NoOfLoopRun {
+    
+    public static void main(String[] args) {   
+        for (int i = 0; i < 7; i++) {            // in each iteration I increase 3 times // 0 -> 3
+            System.out.println("Hello World");
+            i += 2;
+        }
+    }
+}
