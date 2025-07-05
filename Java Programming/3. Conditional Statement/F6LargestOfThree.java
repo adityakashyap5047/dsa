960dd453208534f73c40094e5188e8b0bffa0e17
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-05 14:15:44 +0530

    calculating the maximum of three number in java

---
+++ Java Programming/3. Conditional Statement/F6LargestOfThree.java
@@ -0,0 +1,18 @@
+public class F6LargestOfThree {
+    
+    public static void main(String[] args) {
+        
+        int A = 5, B = 9, C = 10;
+        int max;
+
+        if (A > B && A > C) {
+            max = A;
+        } else if(B > A && B > C){
+            max = B;
+        } else {
+            max = C;
+        }
+
+        System.out.println("The maximum of " + A + ", " + B + " and " + C + " is: " + max);
+    }
+}
