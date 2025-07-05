90f5cf937e820aae1144b104fd23796032494be3
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-05 12:34:59 +0530

    finding output in java

---
+++ Java Programming/2. Operators/E5FindExpressionOutput.java
@@ -0,0 +1,11 @@
+public class E5FindExpressionOutput {
+    
+    public static void main(String[] args){
+        int x = 2, y = 5;
+
+        int exp1 = (x * y / x); // (2 * 5 ) / 2 = 10 / 2 = 5
+        int exp2 = (x * (y / x)); // 2 * (5 / 2) = 2 * 2 = 4
+        System.out.print(exp1 + " , ");
+        System.out.print(exp2);
+    }
+}
