0e6c8a3bc19e8dbbc7358f3e1fb5e2a48b7ad48b
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-05 11:57:23 +0530

    increment operator in java

--- Java Programming/2. Operators/A1ArithmeticOperator.java
+++ Java Programming/2. Operators/A1ArithmeticOperator.java
@@ -9,5 +9,13 @@ public class A1ArithmeticOperator {
         System.out.println("Multiplication: " + (A * B));
         System.out.println("Division: " + (A / B));
         System.out.println("Modulus: " + (A % B));
+
+        int C = ++A;
+        System.out.println("C with Pre-increment: " + C);
+        System.out.println("Value of A after Pre-increment: " + A);
+
+        int D = B++;
+        System.out.println("D with Post-increment: " + D);
+        System.out.println("Value of B after Post-increment: " + B);
     }
 }
