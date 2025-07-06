38515d6d1aeacd8b82a566b84c1c0f80a214ffcc
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-06 09:08:42 +0530

    printing the 0-1 triangle

--- Java Programming/5. Pattern - 1/H5FloydsTriangle.java
+++ Java Programming/5. Pattern - 1/H5FloydsTriangle.java
@@ -1,4 +1,14 @@
 public class H5FloydsTriangle {
+
+    /*
+     
+        1 
+        2 3 
+        4 5 6 
+        7 8 9 10 
+        11 12 13 14 15
+
+    */
     
     public static void main(String[] args) {
         int n = 5;
