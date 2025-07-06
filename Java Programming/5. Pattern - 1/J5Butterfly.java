0a88463c82b42c5c6b4813278ebd0a97c9948836
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-06 09:42:53 +0530

    printing the butterfly pattern

---
+++ Java Programming/5. Pattern - 1/J5Butterfly.java
@@ -0,0 +1,36 @@
+public class J5Butterfly {
+    
+    public static void main(String[] args) {
+        int n = 4;
+
+        for (int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++){
+                if (j <= i) {
+                    System.out.print('*');
+                } 
+            }
+            for (int j = 1; j <= n; j++) {
+                if (i + j >= n + 1) {
+                    System.out.print('*');
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= n; i++) {
+            for(int j = n; j >= i; j--){
+                System.out.print("*"); 
+            }
+            for (int j = 1; j <= n; j++) {
+                if (i <= j) {
+                    System.out.print('*');
+                } else{
+                    System.out.print("  ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
