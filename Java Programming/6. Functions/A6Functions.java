c04df1dff6f0884ceb5ea079e21f7d2d4ae4c781
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-06 18:45:43 +0530

    functions in java

---
+++ Java Programming/6. Functions/A6Functions.java
@@ -0,0 +1,11 @@
+public class A6Functions{
+
+    public static void printHW(){
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+
+        printHW();
+    }
+}
