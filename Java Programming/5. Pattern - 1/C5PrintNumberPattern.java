04b0d58c0250647a876e45832d49b01f381ea236
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-06 08:00:06 +0530

    print number pattern

---
+++ Java Programming/5. Pattern - 1/C5PrintNumberPattern.java
@@ -0,0 +1,22 @@
+public class C5PrintNumberPattern {
+    
+    /*
+        
+        1
+        1 2
+        1 2 3
+        1 2 3 4
+
+    */
+
+    public static void main(String[] args) {
+        int num = 4;
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
