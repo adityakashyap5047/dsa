e31011e70a0a96f3bed326c17ce029f5d26d09f5
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-07 10:49:41 +0530

    taking input and output with array

--- Data Structure/1. Array/B2InputInArray.java
+++ Data Structure/1. Array/B2InputInArray.java
@@ -1,4 +1,27 @@
+import java.util.Scanner;
+
 public class B2InputInArray {
     
-    
+    public static void main(String[] args) {
+        
+        int marks[] = new int[40];
+
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+        for (int i = 1; i <= 3; i++) {
+            System.out.print("Enter elements of Array: ");
+            num = sc.nextInt();
+
+            marks[i] = num;
+        }
+
+        System.out.print("The array is: ");
+        for (int i = 1; i <= 3; i++) {
+            System.out.print(marks[i] + " ");
+        }
+        System.out.println();
+
+        sc.close();
+    }
 }
