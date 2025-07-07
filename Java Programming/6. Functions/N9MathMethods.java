7ef4ba7231ec2183fd5f5c6080978c8fcacf5fc4
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-07 09:29:08 +0530

    inbuilt methods in maths library

---
+++ Java Programming/6. Functions/N9MathMethods.java
@@ -0,0 +1,12 @@
+public class N9MathMethods {
+    
+    public static void main(String[] args) {
+        int num1 = 9, num2 = 16, num3 = 25;
+
+        System.out.println(Math.min(num1, Math.min(num2, num3)));
+        System.out.println(Math.max(num1, Math.max(num2, num3)));
+        System.out.println(Math.sqrt(num1));
+        System.out.println(Math.pow(num1, 2));
+        System.out.println(Math.abs(-6.4));
+    }
+}
