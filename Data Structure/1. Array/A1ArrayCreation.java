186be0fd0983f0c96273267c20b3c2e9a38b963f
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-07 10:46:15 +0530

    array in java

--- Data Structure/1. Array/A1ArrayCreation.java
+++ Data Structure/1. Array/A1ArrayCreation.java
@@ -3,6 +3,9 @@ public class A1ArrayCreation{
     public static void main(String[] args) {
         
         int marks[] = new int[50];
-        int numbers = {2, 3, 5, 7, 11};
+        int numbers[] = {2, 3, 5, 7, 11};
+
+        System.out.println(marks[1]);
+        System.out.println(numbers[1]);
     }
 }
