b2c45c347c627e843c16955e0fa5704a1e3ab9da
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-07 15:13:47 +0530

    Finding the maximum sub array sum

--- Data Structure/1. Array/I9Subarray.java
+++ Data Structure/1. Array/I9Subarray.java
@@ -1,7 +1,7 @@
 public class I9Subarray {
     
     public static int printSubarrays(int arr[]){
-
+        System.out.println("****************************************************");
         int max = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = i; j < arr.length; j++) {
@@ -17,6 +17,7 @@ public class I9Subarray {
             }
             System.out.println();
         } 
+        System.out.println("*****************************************");
         
         return max;
     }
@@ -24,5 +25,9 @@ public class I9Subarray {
     public static void main(String[] args) {
         int arr[] = {2, 4, 6, 8, 10};
         System.out.println(printSubarrays(arr));
+        int num[] = {1, -2, 6, -1, 3};
+        System.out.println(printSubarrays(num));
+        int arr2[] = {20, -3, -4, -1};
+        System.out.println(printSubarrays(arr2));
     }
 }
