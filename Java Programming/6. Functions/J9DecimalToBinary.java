408b0a1c5ad973cb294317b017cf846cd50bae31
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-07 08:42:57 +0530

    converting from decimal to binary

--- Java Programming/6. Functions/J9DecimalToBinary.java
+++ Java Programming/6. Functions/J9DecimalToBinary.java
@@ -2,16 +2,19 @@ public class J9DecimalToBinary {
     
     public static int decimalToBinary(int num){
          
-        int temp = num, rem, binary = 0;
+        int temp = num, rem;
+        String binary = "";
         while (temp > 0) {
             rem = temp % 2;
-            binary = binary * 10 + rem;
+            binary = Integer.toString(rem) + binary;
             temp = temp / 2;
         }
-        return binary;
+        return Integer.parseInt(binary);
     }
 
     public static void main(String[] args) {
         System.out.println(decimalToBinary(156));
+        System.out.println(decimalToBinary(15));
+        System.out.println(decimalToBinary(4));
     }
 }
