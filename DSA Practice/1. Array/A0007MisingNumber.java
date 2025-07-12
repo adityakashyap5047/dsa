271ef658bbd6f279af267b02e1897a6f5db5cd67
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-12 13:39:37 +0530

    finding the number by 2nd method

--- DSA Practice/1. Array/A0007MisingNumber.java
+++ DSA Practice/1. Array/A0007MisingNumber.java
@@ -12,8 +12,25 @@ public class A0007MisingNumber {
         return totalSum;
     }
 
+    public static int missingNumber(int arr[]){
+        int n = arr.length;
+
+        boolean num[] = new boolean[n+1];
+        for (int i = 0; i < n; i++) {
+            num[arr[i]-1] = true;
+        }
+
+        for (int i = 1; i < num.length; i++) {
+            if (!num[i]) {
+                return i+1;
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 5, 4};
-        System.out.println(missingNum(arr));
+        int arr[] = {1, 2, 4, 5, 6};
+        System.out.println(missingNumber(arr));
     }
 }
