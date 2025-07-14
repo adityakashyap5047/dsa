d680f50d2a302a1e421f830b055e415ef764bee6
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-07-14 10:55:47 +0530

    finding the 1st and last occurance

--- DSA Practice/1. Array/A00181stnlastOccurance.java
+++ DSA Practice/1. Array/A00181stnlastOccurance.java
@@ -12,30 +12,31 @@ public class A00181stnlastOccurance {
             int mid = si + (ei - si)/2;
 
             if (arr[mid] == target) {
-                while (arr[mid - 1] == target) {
+                while (mid > 0 && arr[mid - 1] == target) {
                     mid = mid - 1;
                 }
                 list.add(mid);
-                while (arr[mid + 1] == target) {
+                while (mid < arr.length - 1 && arr[mid + 1] == target) {
                     mid = mid + 1;
                 }
                 list.add(mid);
                 return list;
             } else if (arr[mid] > target) {
-                si = mid + 1;
-            } else {
                 ei = mid - 1;
+            } else {
+                si = mid + 1;
             }
         }
         
+        list.add(-1);
         list.add(-1);
         return list;
     }
 
     public static void main(String[] args) {
         
-        int arr[] = {1, 3, 5, 5, 5, 5, 67, 123, 125};
-        ArrayList<Integer> list = find(arr, 5);
+        int arr[] = {5, 7, 7, 7, 8, 8, 8};
+        ArrayList<Integer> list = find(arr, 8);
 
         for (int i = 0; i < list.size(); i++) {
             System.out.print(list.get(i) + " ");
